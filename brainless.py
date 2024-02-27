67f200652a58a731291c3efd12f6bb02d944903f
xTarzx <telmomartins2000@gmail.com>
2024-02-27 01:32:12 +0000

    refactor: now theres loops all over haha (end my suffering pls)

--- brainless.py
+++ brainless.py
@@ -3,7 +3,7 @@ from brain import Bot, Cell, Grid, Action, Direction, Projectile
 from copy import deepcopy
 
 
-from example_bots import DumbBot, GeminiBot1
+from example_bots import TestBot1, TestBot2
 
 
 class Brainless:
@@ -12,13 +12,14 @@ class Brainless:
     def __init__(self, grid: Grid, bot1: Bot, bot2: Bot):
 
         self.grid = grid
+        self.bots = [bot1, bot2]
         self.bot_dirs = self.grid.place_bots(bot1, bot2)
         self.projectiles: list[Projectile] = []
 
         self.viewport = pygame.Surface(
             (self.grid.x_count * self.grid.cell_size, self.grid.y_count * self.grid.cell_size))
 
-    def execute_action(self, bot: Bot, action: Action) -> pygame.Vector2:
+    def execute_action(self, bot: Bot, action: Action) -> tuple[pygame.Vector2, list[Projectile]]:
         """
         return new position and shoot
         """
@@ -80,66 +81,74 @@ class Brainless:
 
             new_projectiles.append(
                 Projectile(new_projectile_pos.x, new_projectile_pos.y, projectile.direction))
-        bot1_action = bot1.next_action(
-            deepcopy(self.grid), self.bot_dirs, deepcopy(new_projectiles))
-        bot2_action = bot2.next_action(
-            deepcopy(self.grid), self.bot_dirs, deepcopy(new_projectiles))
 
-        bot1_crashed = False
-        bot2_crashed = False
+        bots_actions: dict[str, Action] = {}
+        bots_crashed: dict[str, bool] = {}
+        bots_new_pos: dict[str, pygame.Vector2] = {}
+        bots_projectiles: dict[str, list[Projectile]] = {}
 
-        bot1_cell: Cell = self.grid.grid[self.grid.get_bot_cell_idx(bot1)]
-        bot1_pos = pygame.Vector2(bot1_cell.x, bot1_cell.y)
-        bot1_new_pos, bot1_projectiles = self.execute_action(
-            bot1, bot1_action)
+        for bot in self.bots:
+            bots_actions[bot.name] = bot.next_action(
+                deepcopy(self.grid), self.bot_dirs, deepcopy(new_projectiles))
 
-        bot2_new_pos, bot2_projectiles = self.execute_action(
-            bot2, bot2_action)
-        bot2_cell: Cell = self.grid.grid[self.grid.get_bot_cell_idx(bot2)]
-        bot2_pos = pygame.Vector2(bot2_cell.x, bot2_cell.y)
+        for bot in self.bots:
+            bot_cell: Cell = self.grid.grid[self.grid.get_bot_cell_idx(bot)]
+            bot_pos = pygame.Vector2(bot_cell.x, bot_cell.y)
+            bot_new_pos, bot_projectiles = self.execute_action(
+                bot, bots_actions[bot.name])
 
-        new_projectiles.extend(bot1_projectiles)
-        new_projectiles.extend(bot2_projectiles)
+            bots_new_pos[bot.name] = bot_new_pos
+            bots_projectiles[bot.name] = bot_projectiles
 
-        if bot1_new_pos.x < 0 or bot1_new_pos.x >= self.grid.x_count or bot1_new_pos.y < 0 or bot1_new_pos.y >= self.grid.y_count:
-            bot1_crashed = True
+        for projectiles in bots_projectiles.values():
+            new_projectiles.extend(projectiles)
 
-        if self.grid.cell_at(int(bot1_new_pos.x), int(bot1_new_pos.y)).pit:
-            bot1_crashed = True
+        for bot in self.bots:
+            bot_new_pos = bots_new_pos[bot.name]
 
-        if bot2_new_pos.x < 0 or bot2_new_pos.x >= self.grid.x_count or bot2_new_pos.y < 0 or bot2_new_pos.y >= self.grid.y_count:
-            bot2_crashed = True
+            if bot_new_pos.x < 0 or bot_new_pos.x >= self.grid.x_count or bot_new_pos.y < 0 or bot_new_pos.y >= self.grid.y_count:
+                bots_crashed[bot.name] = True
 
-        if self.grid.cell_at(int(bot2_new_pos.x), int(bot2_new_pos.y)).pit:
-            bot2_crashed = True
+            if self.grid.cell_at(int(bot_new_pos.x), int(bot_new_pos.y)).pit:
+                bots_crashed[bot.name] = True
+
+            for other_bot in self.bots:
+                if bot.name != other_bot.name:
+                    other_bot_new_pos = bots_new_pos[other_bot.name]
+                    if bot_new_pos == other_bot_new_pos:
+                        if bots_actions[bot.name] == Action.FORWARD:
+                            bots_crashed[other_bot.name] = True
+
+                        if bots_actions[other_bot.name] == Action.FORWARD:
+                            bots_crashed[bot.name] = True
 
         for projectile in new_projectiles:
             pos = pygame.Vector2(projectile.x, projectile.y)
-            if pos == bot1_new_pos:
-                bot1_crashed = True
-            if pos == bot2_new_pos:
-                bot2_crashed = True
 
-        if bot1_new_pos == bot2_new_pos:
-            if bot1_action == Action.FORWARD:
-                bot2_crashed = True
-            if bot2_action == Action.FORWARD:
-                bot1_crashed = True
+            for bot in self.bots:
+                bot_new_pos = bots_new_pos[bot.name]
+                if pos == bot_new_pos:
+                    bots_crashed[bot.name] = True
 
-        self.grid.cell_at(int(bot1_pos.x), int(bot1_pos.y)).bot = None
-        self.grid.cell_at(int(bot1_new_pos.x), int(bot1_new_pos.y)).bot = bot1
-        self.grid.cell_at(int(bot2_pos.x), int(bot2_pos.y)).bot = None
-        self.grid.cell_at(int(bot2_new_pos.x), int(bot2_new_pos.y)).bot = bot2
+        for bot in self.bots:
+            bot_cell = self.grid.grid[self.grid.get_bot_cell_idx(bot)]
+            bot_cell.bot = None
+
+            bot_new_pos = bots_new_pos[bot.name]
+
+            bot_new_cell = self.grid.cell_at(
+                int(bot_new_pos.x), int(bot_new_pos.y))
+            bot_new_cell.bot = bot
 
         self.projectiles.clear()
         self.projectiles.extend(new_projectiles)
 
-        if bot1_crashed or bot2_crashed:
-            print(
-                f"bot1_crashed ({bot1.name}): {bot1_crashed} {'loser' if bot1_crashed else ''}")
-            print(
-                f"bot2_crashed ({bot2.name}): {bot2_crashed} {'loser' if bot2_crashed else ''}")
+        if any(bots_crashed.values()):
+            for bot_name, crashed in bots_crashed.items():
+                crashed = "crashed" if crashed else ""
+                print(f"{bot_name} {crashed}")
             return False
+
         return True
 
     def render(self):
@@ -158,8 +167,8 @@ FPS = 60
 
 grid = Grid(13, 13, 40)
 
-bot1 = GeminiBot1("gemini1", (0, 0, 111), (52, 38, 248))
-bot2 = GeminiBot1("gemini2", (111, 0, 13), (220, 38, 148))
+bot1 = TestBot1()
+bot2 = TestBot2()
 
 brainless = Brainless(grid, bot1, bot2)
 
